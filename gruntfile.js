11925f6f5e28205d6daa570c63293116a30fb70e
Joel R Eisner <eisnerjr@vcu.edu>
2016-02-09 09:59:20 -0500

    Added package.json and gruntfile.js to manage the installation of Node.js modules as well as the compiling of SASS

---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // configure tasks
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            options: {
+                livereload: true
+            },
+            css: {
+                files: ['src/sass/*.sass', 'src/sass/**/*.sass'],
+                tasks: ['sass', 'cssmin', 'postcss']
+            }
+        },
+        sass: {
+            options: {
+                sourcemap: 'none'
+            },
+            dist: {
+                files: {
+                    'dist/css/chassis.css': 'src/sass/chassis.sass'
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'dist/css/chassis.min.css': 'dist/css/chassis.css'
+                }
+            }
+        }
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+                    require('autoprefixer')({
+                        browsers: ['last 2 versions']
+                    }),
+                    require('css-mqpacker')()
+                ]
+            },
+            dist: {
+                src: 'dist/css/*.css'
+            }
+        }
+    });
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-postcss');
+    // Default task
+    grunt.registerTask('default', ['watch']);
+    // Build SASS task
+    grunt.registerTask('build-css', ['sass', 'cssmin', 'postcss']);
+};

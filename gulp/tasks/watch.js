adb94fcad9eb38204186a85bf14e18c8e6e2cc12
Joel Eisner <jeisner93@gmail.com>
2019-08-01 20:57:41 -0400

    Updated the watch Gulp task to include exp/min variants, as well as delete the default task from the gulpfile.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,8 +1,21 @@
 const build = require('./build'),
     gulp    = require('gulp');
 
-function watch() {
-    gulp.watch('src/**/*', build.all);
+function watch(style) {
+    const index = style || 'all';
+
+    function method() {
+        gulp.watch('src/**/*', build[index]);
+    }
+
+    if (style) method.displayName = `watch:${ style }`;
+
+    return method;
 }
 
-module.exports = watch;
+module.exports = {
+    all: watch(),
+    exp: watch('exp'),
+    min: watch('min'),
+    ref: watch
+};
